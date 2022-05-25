e8ae52a5b6fd03d0f2daf5a6b30e3786c04e423e
Artur <pufereq@gmail.com>
2022-05-25 13:14:24 +0200

    Initial commit

---
+++ checks/os_check.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import platform
+# os check
+
+os_id = 0
+
+def __init__():
+    os = platform.system().lower()
+    if os == 'linux': os_id = 0
+    elif os == 'windows': os_id = 1
+    else: os_id = 2
+    return os_id
+
+def clear():
+    if os_id == 0: return 'clear'
+    elif os_id == 1: return 'cls'
+    elif os_id == 2: return 'clear'
