027c5dc932db113e9f3957ab0d466fe7b3b15029
Artur <pufereq@gmail.com>
2022-05-25 16:41:15 +0200

    add needs and money; use clear() function

--- dialogs/game.py
+++ dialogs/game.py
@@ -6,9 +6,10 @@
 from rich import print
 from rich.prompt import Prompt
 import os
+import random as rn
 
-from checks.os_check import clear
-from txt.random_data import random_first, random_gender, random_last
+from checks.os_check import clear, clear_dev
+from txt.random_data import random_first, random_gender, random_last, random_deplete, random_money
 
 class Character():
     def __init__(self, first_name, last_name, gender, bio):
@@ -16,6 +17,21 @@ class Character():
         self.last_name = last_name
         self.gender = gender
         self.bio = bio
+
+        self.bladder = 100
+        self.hunger = 100
+        self.energy = 100
+        self.fun = 100
+        self.social = 100
+        self.hygiene = 100
+        self.money = random_money()
+    def deplete_needs(self):
+        self.bladder -= random_deplete()
+        self.hunger -= random_deplete()
+        self.energy -= random_deplete()
+        self.fun -= random_deplete()
+        self.social -= random_deplete()
+        self.hygiene -= random_deplete()
 
 def new_game(debug):
     global main_character
@@ -36,26 +52,36 @@ def new_game(debug):
         panel()
 
 def panel():
-    os.system(clear())
-    print('[bold red]DEBUG[/bold red]')
-    print(f'{main_character.first_name=}\n{main_character.last_name=}\n{main_character.gender=}\n{main_character.bio=}\n\n')
+    clear()
+    # print('[bold red]DEBUG[/bold red]')
+    # print(f'{main_character.first_name=}\n{main_character.last_name=}\n{main_character.gender=}\n{main_character.bio=}\n\n')
 
     # print(f'[bold green]simulat[/bold green]\n[italic yellow]panel[/italic yellow]')
     print(f'''
 [bold green]simulat[/bold green]
   [red]panel:[/red]
     [bold yellow]data:[/bold yellow]
-      [magenta]first name:[/magenta] [italic blue]{main_character.first_name}[/italic blue]
-      [magenta]last name:[/magenta] [italic blue]{main_character.last_name}[/italic blue]
-      [magenta]gender:[/magenta] [italic blue]{main_character.gender}[/italic blue]
-      [magenta]biography:[/magenta] [italic blue]{main_character.bio}[/italic blue]
-    [bold yellow]stats:[/bold yellow]
-      [magenta]bladder:[/magenta] [italic blue]not implemented[/italic blue]
-      [magenta]hunger:[/magenta] [italic blue]not implemented[/italic blue]
-      [magenta]energy:[/magenta] [italic blue]not implemented[/italic blue]
-      [magenta]fun:[/magenta] [italic blue]not implemented[/italic blue]
-      [magenta]social:[/magenta] [italic blue]not implemented[/italic blue]
-      [magenta]hygiene:[/magenta] [italic blue]not implemented[/italic blue]
+      [magenta]first name:[/magenta] [bold italic blue]{main_character.first_name}[/bold italic blue]
+      [magenta]last name:[/magenta] [bold italic blue]{main_character.last_name}[/bold italic blue]
+      [magenta]gender:[/magenta] [bold italic blue]{main_character.gender}[/bold italic blue]
+      [magenta]biography:[/magenta] [bold italic blue]{main_character.bio}[/bold italic blue]
+    [bold yellow]needs:[/bold yellow]
+      [magenta]bladder:[/magenta] [bold italic blue]{main_character.bladder}[/bold italic blue]
+      [magenta]hunger:[/magenta] [bold italic blue]{main_character.hunger}[/bold italic blue]
+      [magenta]energy:[/magenta] [bold italic blue]{main_character.energy}[/bold italic blue]
+      [magenta]fun:[/magenta] [bold italic blue]{main_character.fun}[/bold italic blue]
+      [magenta]social:[/magenta] [bold italic blue]{main_character.social}[/bold italic blue]
+      [magenta]hygiene:[/magenta] [bold italic blue]{main_character.hygiene}[/bold italic blue]
+    [bold yellow]budget:[/bold yellow]
+      [magenta]money:[/magenta] [bold italic blue]${main_character.money}[/bold italic blue]
     [bold yellow]interactions:[/bold yellow]
-      [magenta]NOT IMPLEMENTED[/magenta]
+      [purple]buy:[/purple]
+        [magenta]grocery - grocery store[/magenta]
+      [magenta]COMING SOON[/magenta]
     ''')
+    input()
+    main_character.deplete_needs()
+    panel()
+
+def grocery_store():
+    raise NotImplementedError()
