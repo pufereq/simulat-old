b31d7645e05e312119807d9a90872e6c326520c5
Artur <pufereq@gmail.com>
2022-07-08 13:01:56 +0200

    fix issue #2: os_check not working

--- checks/os_check.py
+++ checks/os_check.py
@@ -3,19 +3,25 @@
 import platform, os
 # os check
 
-os_id = 0
-
 def __init__():
+    global os_id
     os = platform.system().lower()
-    if os == 'linux': os_id = 0
-    elif os == 'windows': os_id = 1
-    else: raise NotImplementedError()
-    return os_id
+    if os == 'linux':
+        os_id = 0
+    elif os == 'windows':
+        os_id = 1
+    else:
+        os_id = 2
 
 def clear_cmd():
-    if os_id == 0: return 'clear'
-    elif os_id == 1: return 'cls'
-    elif os_id == 2: return 'clear'
+    if os_id == 0:
+        return 'clear'
+    elif os_id == 1:
+        return 'cls'
+    elif os_id == 2:
+        return 'clear'
 
 def clear():
+    # temporary fix
+    __init__()
     os.system(clear_cmd())
