7b0556f21dbee014c37c05228f91f68a08f691ef
Artur <pufereq@gmail.com>
2022-10-08 15:57:08 +0200

    change: implement print_header()
    Signed-off-by: Artur <pufereq@gmail.com>

--- data/game.py
+++ data/game.py
@@ -41,18 +41,51 @@ def new_game(debug=False):
         main_character = Character('DEBUG', 'DEBUG', 'DEBUG', 'DEBUG')
         home_fridge = Fridge(10, 10, 10, 10, 10, 10)
     else:
-        clear()
-        print("[bold green]Create a new simulat[/bold green]")
-        first_name = Prompt.ask("[magenta]First name[/magenta]",
-                                default=random_first())
-        last_name = Prompt.ask("[magenta]Last name[/magenta]",
-                               default=random_last())
-        gender = Prompt.ask("[magenta]Gender[/magenta]",
-                            default=random_gender())
-        bio = Prompt.ask("[magenta]Biography[/magenta]",
-                         default='None')
-        if bio == 'None':
-            bio = None
+        from main import start_game
+        first_name = None
+        last_name = None
+        gender = None
+        bio = None
+        while True:
+            prompt = print_header("new game", [], "create a simulat",
+                                  [{'category_name': "simulat data", 'data':
+                                    [{'name': "first name", 'desc': first_name, 'interaction': False},
+                                     {'name': "last name", 'desc': last_name, 'interaction': False},
+                                     {'name': "gender", 'desc': gender, 'interaction': False},
+                                     {'name': "biography", 'desc': bio, 'interaction': False}]}],
+                                  'interactions:',
+                                  [{'category_name': "change data", 'data':
+                                    [{'name': "first", 'desc': "change first name", 'interaction': True},
+                                     {'name': "last", 'desc': "change last name", 'interaction': True},
+                                     {'name': "gender", 'desc': "change gender", 'interaction': True},
+                                     {'name': "bio", 'desc': "change biography", 'interaction': True}]},
+                                   {'category_name': "game", 'data':
+                                    [{'name': "done", 'desc': "start the game", 'interaction': True}]}],
+                                  clear_screen=True, go_back=start_game)
+            if prompt == 'first':
+                print("TIP: you can randomize data by inputing 'random' below.")
+                first_name = Prompt.ask("first name")
+                if first_name == 'random':
+                    first_name = random_first()
+            elif prompt == 'last':
+                print("TIP: you can randomize data by inputing 'random' below.")
+                last_name = Prompt.ask("last name")
+                if last_name == 'random':
+                    last_name = random_last()
+            elif prompt == 'gender':
+                print("TIP: you can randomize data by inputing 'random' below.")
+                gender = Prompt.ask("gender")
+                if gender == 'random':
+                    gender = random_gender()
+            elif prompt == 'bio':
+                bio = Prompt.ask("bio")
+            elif prompt == 'done':
+                if first_name and last_name and gender is not None:
+                    break
+                else:
+                    print_state("create a simulat",
+                                "you have to fill these fields: first name; last name; gender.",
+                                redirect=None, level=2, confirm=False)
 
         main_character = Character(first_name, last_name, gender, bio)
         home_fridge = Fridge(random_fridge(), random_fridge(), random_fridge(),
@@ -64,53 +97,49 @@ def new_game(debug=False):
 
 def grocery_store():
     """Buy foodstuffs."""
-    clear()
-    print(f"""[b green]simulat[/b green]
-  [red]grocery store:[/red]
-   [b yellow]fridge contents:[/b yellow]
-      {home_fridge.fridge_contents()}
-   [b yellow]buy:[/b yellow]
-      [magenta]bread:[/magenta] [b i blue]$3[/b i blue]
-      [magenta]vegetables:[/magenta] [b i blue]$2[/b i blue]
-      [magenta]fruits:[/magenta] [b i blue]$2[/b i blue]
-      [magenta]eggs:[/magenta] [b i blue]$4[/b i blue]
-      [magenta]flour:[/magenta] [b i blue]$1[/b i blue]
-      [magenta]meat:[/magenta] [b i blue]$6[/b i blue]
-   [b yellow]interactions:[/b yellow]
-      [b cyan]buy:[/b cyan]
-        [magenta]bread[/magenta] - [white]buy bread[/white]
-        [magenta]vegetables[/magenta] - [white]buy vegetables[/white]
-        [magenta]fruits[/magenta] - [white]buy fruits[/white]
-        [magenta]eggs[/magenta] - [white]buy eggs[/white]
-        [magenta]flour[/magenta] - [white]buy flour[/white]
-        [magenta]meat[/magenta] - [white]buy meat[/white]
-
-        [magenta]back[/magenta] - [white]back to panel[/white]""")
-    choice = Prompt.ask('grocery store',
-                        choices=['bread', 'vegetables', 'fruits',
-                                 'eggs', 'flour', 'meat', 'back'],
-                        default='back',
-                        show_choices=False)
-    if choice == 'back':
-        panel()
+    fridge_contents = home_fridge.fridge_contents()
+    prompt = print_header("buy", [], "grocery store",
+                          [{'category_name': "fridge contents", 'data':
+                            [{'name': "bread", 'desc': fridge_contents['bread'], 'interaction': False},
+                             {'name': "vegetables", 'desc': fridge_contents['vegetables'], 'interaction': False},
+                             {'name': "fruits", 'desc': fridge_contents['fruits'], 'interaction': False},
+                             {'name': "eggs", 'desc': fridge_contents['eggs'], 'interaction': False},
+                             {'name': "flour", 'desc': fridge_contents['flour'], 'interaction': False},
+                             {'name': "meat", 'desc': fridge_contents['meat'], 'interaction': False}]},
+                           {'category_name': "buy", 'data':
+                            [{'name': "bread", 'desc': "$3", 'interaction': False},
+                             {'name': "vegetables", 'desc': "$2", 'interaction': False},
+                             {'name': "fruits", 'desc': "$2", 'interaction': False},
+                             {'name': "eggs", 'desc': "$4", 'interaction': False},
+                             {'name': "flour", 'desc': "$1", 'interaction': False},
+                             {'name': "meat", 'desc': "$6", 'interaction': False}]}],
+                          'interactions:',
+                          [{'category_name': "buy", 'data':
+                           [{'name': "bread", 'desc': "buy bread", 'interaction': True},
+                            {'name': "vegetables", 'desc': "buy vegetables", 'interaction': True},
+                            {'name': "fruits", 'desc': "buy fruits", 'interaction': True},
+                            {'name': "eggs", 'desc': "buy eggs", 'interaction': True},
+                            {'name': "flour", 'desc': "buy flour", 'interaction': True},
+                            {'name': "meat", 'desc': "buy meat", 'interaction': True}]}],
+                          go_back=panel, clear_screen=True)
 
     quantity = IntPrompt.ask('quantity')
-    if choice == 'bread':
+    if prompt == 'bread':
         home_fridge.add_to_fridge('bread', quantity)
 
-    elif choice == 'vegetables':
+    elif prompt == 'vegetables':
         home_fridge.add_to_fridge('vegetables', quantity)
 
-    elif choice == 'fruits':
+    elif prompt == 'fruits':
         home_fridge.add_to_fridge('fruits', quantity)
 
-    elif choice == 'eggs':
+    elif prompt == 'eggs':
         home_fridge.add_to_fridge('eggs', quantity)
 
-    elif choice == 'flour':
+    elif prompt == 'flour':
         home_fridge.add_to_fridge('flour', quantity)
 
-    elif choice == 'meat':
+    elif prompt == 'meat':
         home_fridge.add_to_fridge('meat', quantity)
     grocery_store()
 
@@ -123,33 +152,31 @@ def panel():
     clear()
     prompt = print_header('game', [], 'panel',
                           [{'category_name': "simulat data", 'data':
-                            [{'name': "first name", 'desc': main_character.first_name},
-                             {'name': "last name", 'desc': main_character.last_name},
-                             {'name': "gender", 'desc': main_character.gender},
-                             {'name': "bio", 'desc': main_character.bio}]},
+                            [{'name': "first name", 'desc': main_character.first_name, 'interaction': False},
+                             {'name': "last name", 'desc': main_character.last_name, 'interaction': False},
+                             {'name': "gender", 'desc': main_character.gender, 'interaction': False},
+                             {'name': "bio", 'desc': main_character.bio, 'interaction': False}]},
                            {'category_name': "needs", 'data':
-                            [{'name': "bladder", 'desc': main_character.bladder},
-                             {'name': "hunger", 'desc': main_character.hunger},
-                             {'name': "energy", 'desc': main_character.energy},
-                             {'name': "fun", 'desc': main_character.fun},
-                             {'name': 'social', 'desc': main_character.social},
-                             {'name': 'hygiene', 'desc': main_character.hygiene}]},
+                            [{'name': "bladder", 'desc': main_character.bladder, 'interaction': False},
+                             {'name': "hunger", 'desc': main_character.hunger, 'interaction': False},
+                             {'name': "energy", 'desc': main_character.energy, 'interaction': False},
+                             {'name': "fun", 'desc': main_character.fun, 'interaction': False},
+                             {'name': 'social', 'desc': main_character.social, 'interaction': False},
+                             {'name': 'hygiene', 'desc': main_character.hygiene, 'interaction': False}]},
                            {'category_name': "budget", 'data':
-                            [{'name': "money", 'desc': f"${main_character.money}"}]}],
-
-                          'interactions:', [],
-
+                            [{'name': "money", 'desc': f"${main_character.money}", 'interaction': False}]}],
+                          'interactions:',
                           [{'category_name': "home", 'data':
-                            [{'name': "fridge", 'desc': 'see contents of the fridge'},
-                             {'name': "bathroom", 'desc': "go to the bathroom"}]},
+                            [{'name': "fridge", 'desc': 'see contents of the fridge', 'interaction': True},
+                             {'name': "bathroom", 'desc': "go to the bathroom", 'interaction': True}]},
                            {'category_name': "money", 'data':
-                            [{'name': "work", 'desc': "go to work"}]},
+                            [{'name': "work", 'desc': "go to work", 'interaction': True}]},
                            {'category_name': "buy", 'data':
-                            [{'name': "grocery", 'desc': "go to the grocery store"}]},
+                            [{'name': "grocery", 'desc': "go to the grocery store", 'interaction': True}]},
                            {'category_name': "game", 'data':
-                            [{'name': "skip", 'desc': "skip a turn"},
-                             {'name': "menu", 'desc': "quit to main menu"},
-                             {'name': "exit", 'desc': "quit simulat"}]}], default='skip')
+                            [{'name': "skip", 'desc': "skip a turn", 'interaction': True},
+                             {'name': "menu", 'desc': "quit to main menu", 'interaction': True},
+                             {'name': "exit", 'desc': "quit simulat", 'interaction': True}]}], default='skip')
     if prompt == 'fridge':
         home_fridge.interact()
 
