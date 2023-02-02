ae8903da6002185d3942ddf788175282105f7624
Artur <pufereq@gmail.com>
2023-02-02 15:31:09 +0100

    rename: game.py > game_panel.py | for future use

--- data/game_panel.py
+++ data/game_panel.py
@@ -8,20 +8,25 @@ Returns:
 
 # imports
 from rich.prompt import Prompt, IntPrompt, Confirm
+import curses as cs
 
 from data.classes.Character.character import Character
 from data.classes.Fridge.fridge import Fridge
 from data.classes.Work.work import Work
 from data.classes.rooms.Bathroom.bathroom import Bathroom
 
-from data.clear import clear
+from data.game.init import top_bar, notification
+
+# from main import height, width
 from data.res.get_names import random_first, random_gender, random_last
 from data.res.random_data import random_fridge
 from data.utils import error_handler, print_header, print_state
+from data.classes.Utilities.windows.menu_handler import Menu
+from data.classes.Utilities.windows.notification import Notification
 
 
-@error_handler
-def new_game(debug=False):
+# @error_handler
+def new_game(game_window, debug=False):
     """Start a new game.
 
     User can choose character details.
@@ -39,58 +44,20 @@ def new_game(debug=False):
         main_character = Character('DEBUG', 'DEBUG', 'DEBUG', 'DEBUG')
         home_fridge = Fridge(10, 10, 10, 10, 10, 10)
     else:
-        from main import start_game
+        # from main import start_game
         first_name = None
         last_name = None
         gender = None
         bio = None
-        while True:
-            prompt = print_header("new game", [], "create a simulat",
-                                  [{'category_name': "simulat data", 'data':
-                                    [{'name': "first name", 'desc': first_name, 'interaction': False},
-                                     {'name': "last name", 'desc': last_name, 'interaction': False},
-                                     {'name': "gender", 'desc': gender, 'interaction': False},
-                                     {'name': "biography", 'desc': bio, 'interaction': False}]}],
-                                  'interactions:',
-                                  [{'category_name': "change data", 'data':
-                                    [{'name': "first", 'desc': "change first name", 'interaction': True},
-                                     {'name': "last", 'desc': "change last name", 'interaction': True},
-                                     {'name': "gender", 'desc': "change gender", 'interaction': True},
-                                     {'name': "bio", 'desc': "change biography", 'interaction': True}]},
-                                   {'category_name': "game", 'data':
-                                    [{'name': "done", 'desc': "start the game", 'interaction': True}]}],
-                                  clear_screen=True, go_back=start_game)
-            if prompt == 'first':
-                print("TIP: you can randomize data by inputing 'random' below.")
-                first_name = Prompt.ask("first name")
-                if first_name == 'random':
-                    first_name = random_first()
-            elif prompt == 'last':
-                print("TIP: you can randomize data by inputing 'random' below.")
-                last_name = Prompt.ask("last name")
-                if last_name == 'random':
-                    last_name = random_last()
-            elif prompt == 'gender':
-                print("TIP: you can randomize data by inputing 'random' below.")
-                gender = Prompt.ask("gender")
-                if gender == 'random':
-                    gender = random_gender()
-            elif prompt == 'bio':
-                bio = Prompt.ask("bio")
-            elif prompt == 'done':
-                if first_name and last_name and gender is not None:
-                    break
-                else:
-                    print_state("create a simulat",
-                                "you have to fill these fields: first name; last name; gender.",
-                                redirect=None, level=2, confirm=False)
+
+        # cs.newwin(game_window.getmaxyx()[0], s)
 
         main_character = Character(first_name, last_name, gender, bio)
         home_fridge = Fridge(random_fridge(), random_fridge(), random_fridge(),
                              random_fridge(), random_fridge(), random_fridge())
     bathroom = Bathroom()
     work = Work()
-    panel()
+    panel(game_window)
 
 
 @error_handler
@@ -149,40 +116,74 @@ def grocery_store():
     grocery_store()
 
 
-@error_handler
-def panel():
+# @error_handler
+def panel(game_window):
     """Game panel.
 
     Shows data, allows to interact with game.
     """
-    clear()
-    prompt = print_header('game', [], 'panel',
-                          [{'category_name': "simulat data", 'data':
-                            [{'name': "first name", 'desc': main_character.first_name, 'interaction': False},
-                             {'name': "last name", 'desc': main_character.last_name, 'interaction': False},
-                             {'name': "gender", 'desc': main_character.gender, 'interaction': False},
-                             {'name': "bio", 'desc': main_character.bio, 'interaction': False}]},
-                           {'category_name': "needs", 'data':
-                            [{'name': "bladder", 'desc': main_character.bladder, 'interaction': False},
-                             {'name': "hunger", 'desc': main_character.hunger, 'interaction': False},
-                             {'name': "energy", 'desc': main_character.energy, 'interaction': False},
-                             {'name': "fun", 'desc': main_character.fun, 'interaction': False},
-                             {'name': 'social', 'desc': main_character.social, 'interaction': False},
-                             {'name': 'hygiene', 'desc': main_character.hygiene, 'interaction': False}]},
-                           {'category_name': "budget", 'data':
-                            [{'name': "money", 'desc': f"${main_character.money}", 'interaction': False}]}],
-                          'interactions:',
-                          [{'category_name': "home", 'data':
-                            [{'name': "fridge", 'desc': 'see contents of the fridge', 'interaction': True},
-                             {'name': "bathroom", 'desc': "go to the bathroom", 'interaction': True}]},
-                           {'category_name': "money", 'data':
-                            [{'name': "work", 'desc': "go to work", 'interaction': True}]},
-                           {'category_name': "buy", 'data':
-                            [{'name': "grocery", 'desc': "go to the grocery store", 'interaction': True}]},
-                           {'category_name': "game", 'data':
-                            [{'name': "skip", 'desc': "skip a turn", 'interaction': True},
-                             {'name': "menu", 'desc': "quit to main menu", 'interaction': True},
-                             {'name': "exit", 'desc': "quit simulat", 'interaction': True}]}], default='skip')
+    game_window.clear()
+    game_window.refresh()
+    height, width = game_window.getmaxyx()
+    top_bar.update_details(' ')
+
+    info_panel_border = cs.newwin(height - 1, 31, 1, 0)
+    info_panel_border.border()
+    info_panel_border.refresh()
+    info_panel = cs.newwin(height - 1, 31, 2, 1)
+    info_panel.addstr(f"First Name: {main_character.first_name}")
+
+    menu = Menu(
+        'panelll',
+        [
+            {
+                'name': "test",
+                'label': "test",
+                'target': top_bar.update_details,
+                'args': ['lady gagaga']
+            },
+            {
+                'name': 'foo',
+                'label': 'foo1',
+                'target': notification,
+                'args': {'new_title': f"{''.join(f'#' for i in range(50))}"}
+            }
+        ],
+        info_panel, 'bottom'
+    )
+    menu.display()
+
+    info_panel.getch()
+    exit()
+    # clear()
+    # prompt = print_header('game', [], 'panel',
+    #                       [{'category_name': "simulat data", 'data':
+    #                         [{'name': "first name", 'desc': main_character.first_name, 'interaction': False},
+    #                          {'name': "last name", 'desc': main_character.last_name, 'interaction': False},
+    #                          {'name': "gender", 'desc': main_character.gender, 'interaction': False},
+    #                          {'name': "bio", 'desc': main_character.bio, 'interaction': False}]},
+    #                        {'category_name': "needs", 'data':
+    #                         [{'name': "bladder", 'desc': main_character.bladder, 'interaction': False},
+    #                          {'name': "hunger", 'desc': main_character.hunger, 'interaction': False},
+    #                          {'name': "energy", 'desc': main_character.energy, 'interaction': False},
+    #                          {'name': "fun", 'desc': main_character.fun, 'interaction': False},
+    #                          {'name': 'social', 'desc': main_character.social, 'interaction': False},
+    #                          {'name': 'hygiene', 'desc': main_character.hygiene, 'interaction': False}]},
+    #                        {'category_name': "budget", 'data':
+    #                         [{'name': "money", 'desc': f"${main_character.money}", 'interaction': False}]}],
+    #                       'interactions:',
+    #                       [{'category_name': "home", 'data':
+    #                         [{'name': "fridge", 'desc': 'see contents of the fridge', 'interaction': True},
+    #                          {'name': "bathroom", 'desc': "go to the bathroom", 'interaction': True}]},
+    #                        {'category_name': "money", 'data':
+    #                         [{'name': "work", 'desc': "go to work", 'interaction': True}]},
+    #                        {'category_name': "buy", 'data':
+    #                         [{'name': "grocery", 'desc': "go to the grocery store", 'interaction': True}]},
+    #                        {'category_name': "game", 'data':
+    #                         [{'name': "skip", 'desc': "skip a turn", 'interaction': True},
+    #                          {'name': "menu", 'desc': "quit to main menu", 'interaction': True},
+    #                          {'name': "exit", 'desc': "quit simulat", 'interaction': True}]}], default='skip')
+
     if prompt == 'fridge':
         home_fridge.interact()
 
