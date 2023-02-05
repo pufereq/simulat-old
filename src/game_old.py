038bae10b8eb74d6e691c3180f84f0a81af5205f
Artur <pufereq@gmail.com>
2023-02-05 18:37:06 +0100

    rename: data > src

--- src/game_old.py
+++ src/game_old.py
@@ -10,19 +10,19 @@ Returns:
 from rich.prompt import Prompt, IntPrompt, Confirm
 import curses as cs
 
-from data.classes.Character.character import Character
-from data.classes.Fridge.fridge import Fridge
-from data.classes.Work.work import Work
-from data.classes.rooms.Bathroom.bathroom import Bathroom
+from src.classes.Character.character import Character
+from src.classes.Fridge.fridge import Fridge
+from src.classes.Work.work import Work
+from src.classes.rooms.Bathroom.bathroom import Bathroom
 
-from data.game.init import top_bar, notification
+from src.game.init import top_bar, notification
 
 # from main import height, width
-from data.res.get_names import random_first, random_gender, random_last
-from data.res.random_data import random_fridge
-from data.utils import error_handler, print_header, print_state
-from data.classes.Utilities.windows.menu_handler import Menu
-from data.classes.Utilities.windows.notification import Notification
+from src.res.get_names import random_first, random_gender, random_last
+from src.res.random_data import random_fridge
+from src.utils import error_handler, print_header, print_state
+from src.classes.Utilities.windows.menu_handler import Menu
+from src.classes.Utilities.windows.notification import Notification
 
 
 # @error_handler
@@ -65,14 +65,14 @@ def grocery_store():
     """Buy foodstuffs."""
     fridge_contents = home_fridge.fridge_contents()
     prompt = print_header("buy", [], "grocery store",
-                          [{'category_name': "fridge contents", 'data':
+                          [{'category_name': "fridge contents", 'src':
                             [{'name': "bread", 'desc': fridge_contents['bread'], 'interaction': False},
                              {'name': "vegetables", 'desc': fridge_contents['vegetables'], 'interaction': False},
                              {'name': "fruits", 'desc': fridge_contents['fruits'], 'interaction': False},
                              {'name': "eggs", 'desc': fridge_contents['eggs'], 'interaction': False},
                              {'name': "flour", 'desc': fridge_contents['flour'], 'interaction': False},
                              {'name': "meat", 'desc': fridge_contents['meat'], 'interaction': False}]},
-                           {'category_name': "buy", 'data':
+                           {'category_name': "buy", 'src':
                             [{'name': "bread", 'desc': "$3", 'interaction': False},
                              {'name': "vegetables", 'desc': "$2", 'interaction': False},
                              {'name': "fruits", 'desc': "$2", 'interaction': False},
@@ -80,7 +80,7 @@ def grocery_store():
                              {'name': "flour", 'desc': "$1", 'interaction': False},
                              {'name': "meat", 'desc': "$6", 'interaction': False}]}],
                           'interactions:',
-                          [{'category_name': "buy", 'data':
+                          [{'category_name': "buy", 'src':
                            [{'name': "bread", 'desc': "buy bread", 'interaction': True},
                             {'name': "vegetables", 'desc': "buy vegetables", 'interaction': True},
                             {'name': "fruits", 'desc': "buy fruits", 'interaction': True},
@@ -120,7 +120,7 @@ def grocery_store():
 def panel(game_window):
     """Game panel.
 
-    Shows data, allows to interact with game.
+    Shows src, allows to interact with game.
     """
     game_window.clear()
     game_window.refresh()
@@ -157,29 +157,29 @@ def panel(game_window):
     exit()
     # clear()
     # prompt = print_header('game', [], 'panel',
-    #                       [{'category_name': "simulat data", 'data':
+    #                       [{'category_name': "simulat src", 'src':
     #                         [{'name': "first name", 'desc': main_character.first_name, 'interaction': False},
     #                          {'name': "last name", 'desc': main_character.last_name, 'interaction': False},
     #                          {'name': "gender", 'desc': main_character.gender, 'interaction': False},
     #                          {'name': "bio", 'desc': main_character.bio, 'interaction': False}]},
-    #                        {'category_name': "needs", 'data':
+    #                        {'category_name': "needs", 'src':
     #                         [{'name': "bladder", 'desc': main_character.bladder, 'interaction': False},
     #                          {'name': "hunger", 'desc': main_character.hunger, 'interaction': False},
     #                          {'name': "energy", 'desc': main_character.energy, 'interaction': False},
     #                          {'name': "fun", 'desc': main_character.fun, 'interaction': False},
     #                          {'name': 'social', 'desc': main_character.social, 'interaction': False},
     #                          {'name': 'hygiene', 'desc': main_character.hygiene, 'interaction': False}]},
-    #                        {'category_name': "budget", 'data':
+    #                        {'category_name': "budget", 'src':
     #                         [{'name': "money", 'desc': f"${main_character.money}", 'interaction': False}]}],
     #                       'interactions:',
-    #                       [{'category_name': "home", 'data':
+    #                       [{'category_name': "home", 'src':
     #                         [{'name': "fridge", 'desc': 'see contents of the fridge', 'interaction': True},
     #                          {'name': "bathroom", 'desc': "go to the bathroom", 'interaction': True}]},
-    #                        {'category_name': "money", 'data':
+    #                        {'category_name': "money", 'src':
     #                         [{'name': "work", 'desc': "go to work", 'interaction': True}]},
-    #                        {'category_name': "buy", 'data':
+    #                        {'category_name': "buy", 'src':
     #                         [{'name': "grocery", 'desc': "go to the grocery store", 'interaction': True}]},
-    #                        {'category_name': "game", 'data':
+    #                        {'category_name': "game", 'src':
     #                         [{'name': "skip", 'desc': "skip a turn", 'interaction': True},
     #                          {'name': "menu", 'desc': "quit to main menu", 'interaction': True},
     #                          {'name': "exit", 'desc': "quit simulat", 'interaction': True}]}], default='skip')
